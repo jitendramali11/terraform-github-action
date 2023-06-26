e142933d1efae167038a4a7f28f9be37af255433
Jitendra Mali <jitendra.mali@capgemini.com>
2023-06-26 22:25:13 +0530

    updated

--- src/module/main.tf
+++ src/module/main.tf
@@ -3,16 +3,22 @@ resource "azurerm_resource_group" "rgname" {
   location = var.resource_group_location
 }
 
-module "azurerm_storage_account" {
-  source = "./Modules/Storage Account"
+module "stg" {
+  source = "../module/Storage Account"
   storage_account = "demoterraformstr"
   resource_group_name_prefix = "terraform-rg"
   resource_group_location = "North Europe"
 }
 
-module "azurerm_key_vault" {
-  source = "./Modules/KeyVault"
+module "keyva" {
+  source = "../module/KeyVault"
   keyvaultname =  "demoterraformkv"
   resource_group_name_prefix = "terraform-rg"
+  resource_group_location = "North Europe"
+}
+
+module "example" {
+  source = "../module/sql-sqlserver"
+   resource_group_name_prefix= "terraform-rg"
   resource_group_location = "North Europe"
 }
