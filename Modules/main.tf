30dc2e71e3d3b95748de1d8ff7346dc79df488ae
Jitendra Mali <jitendra.mali@capgemini.com>
2023-06-30 16:05:59 +0530

    updated

--- Modules/main.tf
+++ Modules/main.tf
@@ -4,21 +4,21 @@ resource "azurerm_resource_group" "rgname" {
 }
 
 module "azurerm_storage_account" {
-  source = "./Modules/storage-account"
+  source = "./storage-account"
   storage_account = "pihustr"
   resource_group_name_prefix = azurerm_resource_group.rgname.name
   resource_group_location = "North Europe"
 }
 
 module "azurerm_key_vault" {
-  source = "./Modules/keyvault"
+  source = "./keyvault"
   keyvaultname =  "kvpihu"
   resource_group_name_prefix = azurerm_resource_group.rgname.name
   resource_group_location = "North Europe"
 }
 
 module "azurerm_mssql_server" {
-  source = "./Modules/sql-server"
+  source = "./sql-server"
    resource_group_name_prefix= azurerm_resource_group.rgname.name
   resource_group_location = "North Europe"
 }
